2a906016ec141787348b06f6e848058081aa372a
viveks180 <prajoshi12345@gmail.com>
2018-06-12 16:28:56 -0400

    Spi cam on raspberry pi
    Signed-off-by: viveks180 <prajoshi12345@gmail.com>

---
+++ image.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import time
+import datetime 
+from datetime import timedelta
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+COMMASPACE = ', '
+
+def main():
+    sender = 'MAIL'
+    gmail_password = 'PASSWORD'
+    recipients = ['CAMERA_ACCOUNT']
+    f_time = datetime.datetime.now().strftime('%a %d %b %H:%M')
+    subject = 'Photo @' + f_time
+    # Create the enclosing (outer) message
+    outer = MIMEMultipart()
+    outer['Subject'] = subject
+    outer['To'] = COMMASPACE.join(recipients)
+    outer['From'] = sender
+    outer.preamble = 'Photo' + f_time
+
+    # List of attachments
+    filevivek = '/mnt/motionvideos/' + datetime.datetime.now().strftime('%Y%m%d%H%M.jpg')
+    filevivek2 = '/mnt/motionvideos/' + ((datetime.datetime.now() - datetime.timedelta(minutes = 1)).strftime('%Y%m%d%H%M.jpg'))
+    attachments = [filevivek, filevivek2]
+    deletepath = 'sudo rm -r ' + filevivek
+    deletepath2 = 'sudo rm -r ' + filevivek2
+    
+
+    # Add the attachments to the message
+    for file in attachments:
+        try:
+            with open(file, 'rb') as fp:
+                msg = MIMEBase('application', "octet-stream")
+                msg.set_payload(fp.read())
+            encoders.encode_base64(msg)
+            msg.add_header('Content-Disposition', 'attachment', filename=os.path.basename(file))
+            outer.attach(msg)
+        except:      
+            continue
+            
+    composed = outer.as_string()
+
+    # Send the email
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as s:
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(sender, gmail_password)
+            s.sendmail(sender, recipients, composed)
+            s.close()
+        print("Email sent!")
+    except:
+        print("not mail" + file)	        
+   
+
+    try:
+        os.remove(filevivek)
+    except:
+        os.remove(filevivek2)
+    
+
+if __name__ == '__main__':
+    main()
---
+++ notif.py
@@ -0,0 +1,6 @@
+import os
+from pushetta import Pushetta
+API_KEY="apikey"
+CHANNEL_NAME="channel"
+p=Pushetta(API_KEY)
+p.pushMessage(CHANNEL_NAME, "Intruder found!")
